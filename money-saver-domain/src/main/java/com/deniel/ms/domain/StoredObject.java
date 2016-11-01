c0f52c5b76fe110a2466cfd79124c35104db5a63
Deniel089 <deniel089@gmail.com>
2016-11-01 12:25:02 +0300

    money-saver: Added abstract StoredObject.class. User.class extends StoredObject.class.

---
+++ money-saver-domain/src/main/java/com/deniel/ms/domain/StoredObject.java
@@ -0,0 +1,46 @@
+package com.deniel.ms.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by DenielNote on 01.11.2016.
+ */
+public abstract class StoredObject implements Serializable{
+    private String id;
+    private String name;
+    private Date createDate;
+    private Date updateDate;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+}
--- money-saver-domain/src/main/java/com/deniel/ms/domain/user/User.java
+++ money-saver-domain/src/main/java/com/deniel/ms/domain/user/User.java
@@ -1,35 +1,14 @@
 package com.deniel.ms.domain.user;
 
-import java.io.Serializable;
-import java.util.Date;
+import com.deniel.ms.domain.StoredObject;
 
 /**
  * Created by DenielNote on 01.11.2016.
  */
-public class User implements Serializable{
-    private String id;
-    private String name;
-    private String login;
+public class User extends StoredObject {
+    private String login ;
     private String pass;
-    private Date createDate;
-    private Date updateDate;
     private boolean activeFlag;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public String getLogin() {
         return login;
@@ -39,28 +18,12 @@ public class User implements Serializable{
         this.login = login;
     }
 
-    public Date getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
-
     public String getPass() {
         return pass;
     }
 
     public void setPass(String pass) {
         this.pass = pass;
-    }
-
-    public Date getUpdateDate() {
-        return updateDate;
-    }
-
-    public void setUpdateDate(Date updateDate) {
-        this.updateDate = updateDate;
     }
 
     public boolean isActiveFlag() {
