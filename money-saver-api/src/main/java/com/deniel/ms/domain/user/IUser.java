d36fd6dc5d3ac25139e5dfedacd373b0ef07eb83
Deniel089 <deniel089@gmail.com>
2016-11-01 18:20:55 +0300

    money-saver: Added IUser interface.

---
+++ money-saver-api/src/main/java/com/deniel/ms/domain/user/IUser.java
@@ -0,0 +1,20 @@
+package com.deniel.ms.domain.user;
+
+import com.deniel.ms.domain.Identifiable;
+
+/**
+ * Created by DenielNote on 01.11.2016.
+ */
+public interface IUser extends Identifiable<String>{
+    String getLogin();
+
+    void setLogin(String login);
+
+    String getPass();
+
+    void setPass(String pass);
+
+    boolean isActiveFlag();
+
+    void setActiveFlag(boolean activeFlag);
+}
--- money-saver-domain/src/main/java/com/deniel/ms/domain/user/User.java
+++ money-saver-domain/src/main/java/com/deniel/ms/domain/user/User.java
@@ -1,36 +1,36 @@
 package com.deniel.ms.domain.user;
 
-import com.deniel.ms.domain.Identifiable;
 import com.deniel.ms.domain.StoredObject;
 
 /**
  * Created by DenielNote on 01.11.2016.
  */
-public class User extends StoredObject implements Identifiable<String>{
+public class User extends StoredObject implements IUser {
     private String login ;
     private String pass;
     private boolean activeFlag;
 
+    @Override
     public String getLogin() {
         return login;
     }
-
+    @Override
     public void setLogin(String login) {
         this.login = login;
     }
-
+    @Override
     public String getPass() {
         return pass;
     }
-
+    @Override
     public void setPass(String pass) {
         this.pass = pass;
     }
-
+    @Override
     public boolean isActiveFlag() {
         return activeFlag;
     }
-
+    @Override
     public void setActiveFlag(boolean activeFlag) {
         this.activeFlag = activeFlag;
     }
