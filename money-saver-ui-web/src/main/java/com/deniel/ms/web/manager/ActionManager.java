b9b25f49febb6dcd918f290d062072799ed745e4
Deniel089 <deniel089@gmail.com>
2016-12-25 21:49:43 +0300

    money-saver: Small refactoring

--- money-saver-ui-web/src/main/java/com/deniel/ms/web/manager/ActionManager.java
+++ money-saver-ui-web/src/main/java/com/deniel/ms/web/manager/ActionManager.java
@@ -3,7 +3,6 @@ package com.deniel.ms.web.manager;
 import com.deniel.ms.web.action.HomeAction;
 import com.deniel.ms.web.action.NotFoundAction;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,18 +11,24 @@ import java.util.Map;
  * Created by DenielNote on 16.11.2016.
  */
 public class ActionManager {
-    private static final Map<String, IAction> actionList = new HashMap<>();
-    private static ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
-    static {
+    private final Map<String, IAction> actionList = new HashMap<>();
+    private ApplicationContext context;
 
-        actionList.put("homepage", context.getBean("homeaction", HomeAction.class));
+    public ActionManager(ApplicationContext context) {
+        this.context = context;
+        fillActionList();
     }
 
-    public static synchronized IAction findAction(String actionInstance) {
+    public synchronized IAction findAction(String actionInstance) {
         IAction returnAction = actionList.get(actionInstance);
         if (returnAction == null) {
-            return context.getBean("notfoundaction", NotFoundAction.class);
+            return actionList.get("notfoundaction");
         }
         return returnAction;
+    }
+
+    private void fillActionList() {
+        actionList.put("homepage", context.getBean("homeaction", HomeAction.class));
+        actionList.put("notfoundaction", context.getBean("notfoundaction", NotFoundAction.class));
     }
 }
--- money-saver-ui-web/src/main/java/com/deniel/ms/web/servlet/DispatcherServlet.java
+++ money-saver-ui-web/src/main/java/com/deniel/ms/web/servlet/DispatcherServlet.java
@@ -1,14 +1,15 @@
 package com.deniel.ms.web.servlet;
 
 import com.deniel.ms.web.manager.ActionManager;
-import com.deniel.ms.web.manager.IAction;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.io.IOException;
-import java.text.MessageFormat;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.MessageFormat;
 
 /**
  * Created by DenielNote on 13.11.2016.
@@ -16,6 +17,15 @@ import javax.servlet.http.HttpServletResponse;
 public class DispatcherServlet extends HttpServlet {
     private static final String WEBINF_FMT = "/WEB-INF/jsp/{0}.jsp";
     private static final String APP_PATH = "/app/";
+    private ApplicationContext context;
+    private ActionManager actionManager;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        context = new ClassPathXmlApplicationContext("web-context.xml");
+        actionManager = new ActionManager(context);
+    }
 
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         processRequest(req, resp);
@@ -40,6 +50,6 @@ public class DispatcherServlet extends HttpServlet {
     public String getAction(HttpServletRequest req, HttpServletResponse resp) {
         String reqUrl = req.getRequestURI();
         String action = reqUrl.substring(reqUrl.indexOf(APP_PATH) + APP_PATH.length());
-        return ActionManager.findAction(action).doAction(req, resp);
+        return actionManager.findAction(action).doAction(req, resp);
     }
 }
